9beb6af595eba549619ff9b42ad5921c65104b04
Aliaksandr Stsiapanay <astsiapanay@infoblox.com>
2016-01-12 11:39:31 +0300

    implement query function for sample data source plugin;

--- public/app/plugins/datasource/sample/datasource.js
+++ public/app/plugins/datasource/sample/datasource.js
@@ -17,13 +17,13 @@ function (angular) {
     SampleDatasource.prototype._get = function(relativeUrl, params) {
       return backendSrv.datasourceRequest({
         method: 'GET',
-        url: this.url + relativeUrl,
+        url: this.url + (!relativeUrl ? '' : relativeUrl),
         params: params,
       });
     };
 
     SampleDatasource.prototype.testDatasource = function() {
-      return this._get('/countries?per_page=1&incomeLevel=LIC&format=json').then(function() {
+      return this._get().then(function() {
         return { status: "success", message: "Data source is working", title: "Success" };
       }, function(err) {
         if (err.data && err.data.error) {
@@ -34,11 +34,29 @@ function (angular) {
       });
     };
 
-    SampleDatasource.prototype.query = function(options) {
-      return backendSrv.get('/api/metrics/test', {
-        from: options.range.from.valueOf(),
-        to: options.range.to.valueOf(),
-        maxDataPoints: options.maxDataPoints
+    function convert(result) {
+      var map = {};
+      result.data.data.forEach(function(row) {
+        var dp = map[row[9]];
+        if(!dp) {
+          dp = [];
+          map[row[9]] = dp;
+        }
+        dp.push([row[13], (new Date(row[10])).getTime()]);
+      });
+      var dataSeries = [];
+      for (var key in map) {
+        var dp = map[key].sort(function(a,b) {
+          return a[1] - b[1];
+        });
+        dataSeries.push({'target': key, 'datapoints': dp});
+      }
+      return {data: dataSeries};
+    }
+
+    SampleDatasource.prototype.query = function() {
+      return this._get('/f29j-qgmr/rows.json').then(function(result) {
+        return convert(result);
       });
     };
 
