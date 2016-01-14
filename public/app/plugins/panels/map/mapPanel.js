20262b753b0528210ca6ad768f5f3dace46e32a5
Aliaksandr Stsiapanay <astsiapanay@infoblox.com>
2016-01-14 15:43:57 +0300

    visualize data on the world map;

--- public/app/plugins/panels/map/mapPanel.js
+++ public/app/plugins/panels/map/mapPanel.js
@@ -15,10 +15,10 @@ function (angular, app, _, $, L) {
 
     return {
       link: function(scope, elem) {
-        var data, panel, map;
+        var data, panel, map, circles = [];
 
-        scope.$on('render', function() {
-          render();
+        scope.$on('render', function(event, renderData) {
+          render(renderData);
           scope.panelRenderingComplete();
         });
 
@@ -45,25 +45,44 @@ function (angular, app, _, $, L) {
             return;
           }
           L.Icon.Default.imagePath = 'public/vendor/leaflet/dist/images';
-          map = L.map(elem[0]).setView([51.505, -0.09], 13);
+          map = L.map(elem[0]).setView([37.8, -96], 2);
           L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           }).addTo(map);
 
-          L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+//          L.marker([51.5, -0.09]).addTo(map)
+//            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//            .openPopup();
         }
 
-        function render() {
-          if (!scope.data) { return; }
+        function addCircles() {
+          circles.forEach(function(c) {
+            map.removeLayer(c);
+          });
+          circles = [];
+          if(!data) {
+            return;
+          }
+          data.forEach(function(row) {
+            row.datapoints.forEach(function(dp) {
+              var coords = dp[2].coordinates;
+              circles.push(L.circle([coords[0], coords[1]], dp[0]*10000, {
+                color: row.target,
+                fillColor: '#f03',
+                fillOpacity: 0.5
+              }).addTo(map));
+            });
+          });
+        }
 
-          data = scope.data;
+        function render(renderData) {
+          data = renderData;
           panel = scope.panel;
 
           setElementHeight();
 
           initMap();
+          addCircles();
         }
       }
     };
--- public/app/plugins/panels/map/module.js
+++ public/app/plugins/panels/map/module.js
@@ -33,32 +33,6 @@ function (angular, app, _, kbn, TimeSeries, PanelMeta) {
     $scope.panelMeta.addEditorTab('Options', 'public/plugins/map/editor.html');
     $scope.panelMeta.addEditorTab('Time range', 'app/features/panel/partials/panelTime.html');
 
-    // Set and populate defaults
-    var _d = {
-      pieType: 'pie',
-      legend: {
-        show: true, // disable/enable legend
-        legendType: 'rightSide',
-        values: false, // disable/enable legend values
-        min: false,
-        max: false,
-        current: false,
-        total: false,
-        avg: false
-      },
-      links: [],
-      datasource: null,
-      maxDataPoints: 3,
-      interval: null,
-      targets: [{}],
-      cacheTimeout: null,
-      nullText: null,
-      nullPointMode: 'connected'
-    };
-
-    _.defaults($scope.panel, _d);
-    _.defaults($scope.panel.legend, _d.legend);
-
     $scope.unitFormats = kbn.getUnitFormats();
 
     $scope.setUnitFormat = function(subItem) {
@@ -82,114 +56,7 @@ function (angular, app, _, kbn, TimeSeries, PanelMeta) {
     };
 
     $scope.dataHandler = function(results) {
-      $scope.series = _.map(results.data, $scope.seriesHandler);
-      $scope.render();
-    };
-
-    $scope.seriesHandler = function(seriesData) {
-      var series = new TimeSeries({
-        datapoints: seriesData.datapoints,
-        alias: seriesData.target
-      });
-
-      series.flotpairs = series.getFlotPairs($scope.panel.nullPointMode);
-
-      return series;
-    };
-
-    $scope.getDecimalsForValue = function(value) {
-      if (_.isNumber($scope.panel.decimals)) {
-        return { decimals: $scope.panel.decimals, scaledDecimals: null };
-      }
-
-      var delta = value / 2;
-      var dec = -Math.floor(Math.log(delta) / Math.LN10);
-
-      var magn = Math.pow(10, -dec),
-          norm = delta / magn, // norm is between 1.0 and 10.0
-          size;
-
-      if (norm < 1.5) {
-        size = 1;
-      } else if (norm < 3) {
-        size = 2;
-        // special case for 2.5, requires an extra decimal
-        if (norm > 2.25) {
-          size = 2.5;
-          ++dec;
-        }
-      } else if (norm < 7.5) {
-        size = 5;
-      } else {
-        size = 10;
-      }
-
-      size *= magn;
-
-      // reduce starting decimals if not needed
-      if (Math.floor(value) === value) { dec = 0; }
-
-      var result = {};
-      result.decimals = Math.max(0, dec);
-      result.scaledDecimals = result.decimals - Math.floor(Math.log(size) / Math.LN10) + 2;
-
-      return result;
-    };
-
-    $scope.render = function() {
-      var data = [];
-
-      if ($scope.series && $scope.series.length > 0) {
-        for (var i=0; i < $scope.series.length; i++) {
-          data.push({label: $scope.series[i].alias, data: $scope.series[i].stats.current, color: $rootScope.colors[i]});
-        }
-      }
-
-      $scope.data = data;
-      panelHelper.broadcastRender($scope, data);
-    };
-
-    $scope.setValues = function(data) {
-      data.flotpairs = [];
-
-      if ($scope.series && $scope.series.length > 0) {
-        var lastPoint = _.last($scope.series[0].datapoints);
-        var lastValue = _.isArray(lastPoint) ? lastPoint[0] : null;
-
-        if (_.isString(lastValue)) {
-          data.value = 0;
-          data.valueFormated = lastValue;
-          data.valueRounded = 0;
-        } else {
-          data.value = $scope.series[0].stats[$scope.panel.valueName];
-          data.flotpairs = $scope.series[0].flotpairs;
-
-          var decimalInfo = $scope.getDecimalsForValue(data.value);
-          var formatFunc = kbn.valueFormats[$scope.panel.format];
-          data.valueFormated = formatFunc(data.value, decimalInfo.decimals, decimalInfo.scaledDecimals);
-          data.valueRounded = kbn.roundValue(data.value, decimalInfo.decimals);
-        }
-      }
-
-      if (data.value === null || data.value === void 0) {
-        data.valueFormated = "no value";
-      }
-    };
-
-    $scope.removeValueMap = function(map) {
-      var index = _.indexOf($scope.panel.valueMaps, map);
-      $scope.panel.valueMaps.splice(index, 1);
-      $scope.render();
-    };
-
-    $scope.addValueMap = function() {
-      $scope.panel.valueMaps.push({value: '', op: '=', text: '' });
-    };
-
-    $scope.legendValuesOptionChanged = function() {
-      var legend = $scope.panel.legend;
-      legend.values = legend.min || legend.max || legend.avg || legend.current || legend.total;
-      $scope.render();
+      panelHelper.broadcastRender($scope, results.data);
     };
 
     $scope.init();
